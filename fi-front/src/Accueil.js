690750d184c08cd105d87d16be7983d7241fb588
Lynoshk <lynoshk@yahoo.fr>
2023-10-31 14:11:08 +0100

    Mise à jour des textes

--- fi-front/src/Accueil.js
+++ fi-front/src/Accueil.js
@@ -4,7 +4,7 @@ import './App.css';
 function Accueil() {
 
   return (
-    <div class="area overflow-x-hidden bg-blue-200">
+    <div class="area overflow-x-hidden bg-blue-300">
       <ul class="circles">
         <li></li>
         <li></li>
@@ -102,7 +102,7 @@ function Accueil() {
               href="https://www.wasenderpro.com"
               target="_blank"
               title=""
-              class="whitespace-nowrap rounded-xl bg-blue-700 px-5 py-3 font-medium text-white transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-offset-2 hover:bg-yellow-500"
+              class="whitespace-nowrap rounded-xl bg-white px-5 py-3 font-medium text-blue-500 transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-offset-2 hover:bg-yellow-500"
             >
               WASENDERPRO ⇾
             </a>
@@ -275,7 +275,7 @@ function Accueil() {
                 </g>
               </svg>
             </span>
-            <span class="bg-green-300 text-yellow-900 inline-block text-center px-2 py-1 rounded text-md font-semibold">
+            <span class="bg-red-300 text-yellow-900 inline-block text-center px-2 py-1 rounded text-md font-semibold">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-8 w-8 animate-spin"
@@ -292,6 +292,18 @@ function Accueil() {
                 <path
                   fill="#282828"
                   d="M491.237 33.24c5.557 0 9.751 1.048 12.687 3.04c2.936 1.993 5.034 5.138 6.292 9.438c1.258 4.299 1.782 10.17 1.782 17.72v12.269H485.05v3.774l.42 10.381c.314 2.307.839 3.985 1.677 5.033c.84 1.049 2.202 1.573 3.985 1.573c2.412 0 4.09-.943 4.928-2.83c.944-1.888 1.363-5.034 1.468-9.333l13.946.839c.105.629.105 1.468.105 2.516c0 6.606-1.783 11.535-5.453 14.785c-3.67 3.25-8.703 4.928-15.309 4.928c-7.969 0-13.526-2.516-16.672-7.444C471 95 469.322 87.24 469.322 76.86V64.172c.356-17.825 3.491-30.88 21.915-30.932Zm-193.88 1.363v52.533c0 3.146.314 5.453 1.048 6.816c1.489 2.915 5.348 2.17 7.445.734a8.389 8.389 0 0 0 2.831-3.25V34.602h16.043v71.617h-12.583l-1.363-8.808h-.314c-3.46 6.606-8.599 9.961-15.414 9.961c-10.49-.026-13.057-7.584-13.668-15.26l-.04-.541a65.277 65.277 0 0 1-.133-3.492V34.603h16.147Zm82.732 0v52.533c0 3.146.314 5.453 1.048 6.816c1.49 2.915 5.348 2.17 7.445.734a8.389 8.389 0 0 0 2.831-3.25V34.602h16.043v71.617h-12.583l-1.363-8.808h-.314c-3.46 6.606-8.599 9.961-15.414 9.961c-10.49-.026-13.057-7.584-13.668-15.26l-.04-.541a65.278 65.278 0 0 1-.133-3.492V34.603h16.148ZM250.8 33.24c5.243 0 9.542 1.048 12.688 3.25c3.145 2.202 5.557 5.558 7.025 10.171c1.468 4.614 2.202 10.8 2.202 18.455v10.38c0 7.655-.734 13.737-2.202 18.35c-1.468 4.615-3.775 7.97-7.025 10.172c-3.25 2.097-7.655 3.25-13.107 3.25c-5.663.105-10.067-1.048-13.317-3.145c-3.25-2.202-5.558-5.558-6.92-10.171c-1.364-4.614-1.993-10.696-1.993-18.35V65.22c0-7.655.734-13.946 2.307-18.56c1.573-4.718 3.984-8.074 7.34-10.17c3.355-2.098 7.654-3.251 13.002-3.251ZM432.622 4.509v37.748h.105c1.468-2.726 3.355-4.928 5.977-6.606a14.785 14.785 0 0 1 8.283-2.516c3.88 0 6.816 1.048 9.018 3.04c2.202 2.098 3.775 5.348 4.718 9.857c.915 4.368 1.435 10.409 1.467 18.027l.001.743v11.324c0 10.59-1.363 18.455-3.88 23.488c-2.62 5.033-6.605 7.55-12.058 7.55c-3.04 0-5.767-.734-8.283-2.097a14.68 14.68 0 0 1-5.35-5.392l-.208-.376h-.314l-1.678 6.816h-13.317V4.51h15.519Zm-64.173 3.67V21.18h-15.938v85.039h-15.728V21.181H320.74V8.18h47.71Zm-172.909 0l.01.04c.153.681 2.273 10.106 4.605 21.087l.184.87l.278 1.315l.186.883l.093.443l.186.888l.093.445l.185.891l.27 1.303c1.575 7.604 3.078 15.23 3.977 20.698h.42c.861-4.905 2.112-11.182 3.446-17.591l.35-1.674c.176-.838.352-1.676.53-2.512l.274-1.297a2039.272 2039.272 0 0 1 5.597-25.444l.03-.135l.048-.21h16.043l-18.56 66.165v31.771h-15.833V74.448h-.105l-18.35-66.27h16.043Zm54.945 36.175c-2.202 0-3.67 1.154-4.613 3.566c-.944 2.411-1.363 6.081-1.363 11.22v22.334c0 5.243.419 9.122 1.258 11.43c.839 2.306 2.412 3.46 4.718 3.46c2.202 0 3.775-1.154 4.719-3.46c.944-2.308 1.363-6.187 1.363-11.43V59.139c0-5.138-.42-8.913-1.363-11.22c-.944-2.411-2.517-3.565-4.719-3.565Zm184.968 2.098c-1.278 1.18-2.187 2.635-2.728 4.454l-.103.369v40.684c1.3 2.202 3.146 3.67 6.396 3.775c1.678 0 3.04-.629 4.09-1.887c1.048-1.258 1.782-3.356 2.201-6.292c.403-2.818.613-6.7.629-11.644v-9.956c0-5.662-.21-9.961-.524-13.002c-.42-3.041-.943-5.243-1.887-6.501c-1.825-2.622-5.767-2.59-8.074 0Zm55.47-2.412c-1.783.105-3.042.629-3.776 1.573c-.838 1.048-1.363 2.621-1.677 4.928c-.309 2.26-.415 10.16-.42 10.476v5.148h11.744v-4.54v.087c-.007 1.587-.04-.921-.112-4l-.01-.414c-.066-2.706-.162-5.723-.297-6.757c-.315-2.412-.84-4.09-1.678-5.033c-.839-.944-2.097-1.468-3.775-1.468Z"
+                />
+              </svg>
+            </span>
+            <span class="bg-green-300 text-yellow-900 inline-block text-center px-2 py-1 rounded text-md font-semibold">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-8 w-8 animate-spin"
+                viewBox="0 0 12 12"
+              >
+                <path
+                  fill="currentColor"
+                  d="M2 5.248V7.5A1.5 1.5 0 0 0 3.5 9h5A1.5 1.5 0 0 0 10 7.5V5.248L6.205 6.956a.5.5 0 0 1-.41 0L2 5.248Zm.037-1.08L6 5.952l3.963-1.784A1.5 1.5 0 0 0 8.5 3h-5a1.5 1.5 0 0 0-1.463 1.168Z"
                 />
               </svg>
             </span>
@@ -393,7 +405,7 @@ function Accueil() {
                 <span class="text-gray-400 my-3 flex items-center justify-center font-medium uppercase tracking-wider">
                   Explorez le potentiel de nos services
                 </span>
-                <h2 class="block w-full bg-gradient-to-b from-white to-gray-400 bg-clip-text font-bold text-yellow-500 text-3xl sm:text-4xl">
+                <h2 id="services" class="block w-full bg-gradient-to-b from-white to-gray-400 bg-clip-text font-bold text-yellow-500 text-3xl sm:text-4xl">
                   Prêt pour Booster son Business ?
                 </h2>
                 <p class="mx-auto my-4 w-full max-w-xl bg-transparent text-center font-medium leading-relaxed tracking-wide text-gray-400">
@@ -426,12 +438,12 @@ function Accueil() {
                       <line x1="17" y1="17" x2="17" y2="17.01"></line>
                     </svg>
                   </div>
-                  <h3 class="mt-6 text-gray-100 mx-auto rounded-lg bg-indigo-500 font-bold w-48 p-2 text-center">
-                    Service numérique 🕛
+                  <h3 class="mt-6 text-gray-100 mx-auto rounded-lg bg-indigo-500 font-bold  p-2 text-center">
+                    Service numérique & Pub sponsorisée 🕛
                   </h3>
                   <p class="my-4 mb-0 font-normal leading-relaxed tracking-wide text-gray-100">
-                    Digitalisez tout le système de vente de vos produits et
-                    servives
+                    Digitalisez tout le système de vente de vos produits
+                     et optimisez votre présence web
                   </p>
                 </div>
 
@@ -457,7 +469,7 @@ function Accueil() {
                     Messagerie SMS 🕒
                   </h3>
                   <p class="my-4 mb-0 font-normal leading-relaxed tracking-wide text-gray-100">
-                    Porpectez votre marché local et internationnal avec
+                    Prospectez votre marché local et internationnal avec
                     5sender.com
                   </p>
                 </div>
@@ -489,7 +501,7 @@ function Accueil() {
                     WhatSapp Business 🕕
                   </h3>
                   <p class="my-4 mb-0 font-normal leading-relaxed tracking-wide text-gray-100">
-                    Eplorez un service de messagerie Business avec
+                    Explorez un service de messagerie Business avec
                     wasenderpro.com
                   </p>
                 </div>
